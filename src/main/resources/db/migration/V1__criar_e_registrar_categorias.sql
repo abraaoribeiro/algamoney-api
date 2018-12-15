944b991eeb2100af7d4c4c922e234ac43ddbf805
Abraão Souza Ribeiro <abraaohdg@gmail.com>
2018-12-15 21:26:07 -0200

    Tratamento de erros com ExceptionHandler

--- src/main/resources/db/migration/V1__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V1__criar_e_registrar_categorias.sql
@@ -3,8 +3,8 @@ CREATE TABLE  categoria (
 	nome VARCHAR(50) NOT NULL
 ) ENGINE= InnoDB DEFAULT CHARSET=utf8;
 
-INSET INTO categoria (nome) values ('Lazer');
-INSET INTO categoria (nome) values ('Alimentação');
-INSET INTO categoria (nome) values ('Supermercado');
-INSET INTO categoria (nome) values ('Farmácia');
-INSET INTO categoria (nome) values ('Outros');
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
