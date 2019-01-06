bee30e95978173529630ce5a2968b94ff77ae783
Abraão Souza Ribeiro <abraaohdg@gmail.com>
2019-01-06 01:18:47 -0200

    Configurando JWT no projeto

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -45,17 +45,17 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		List<Predicate> predicates = new ArrayList<>();
 
 		if (!StringUtils.isEmpty(lancamentoFilter.getDescricao())) {
-			predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)),
+			predicates.add(builder.like(builder.lower(root.get("descricao")),
 					"%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
 		}
 
 		if (lancamentoFilter.getDataVencimentoDe() != null) {
-			predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataVencimento"),
 					lancamentoFilter.getDataVencimentoDe()));
 		}
 
 		if (lancamentoFilter.getDataVencimentoAte() != null) {
-			predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataVencimento"),
 					lancamentoFilter.getDataVencimentoAte()));
 		}
 
